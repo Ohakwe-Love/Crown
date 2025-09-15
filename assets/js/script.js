aa7a2c581ae8cb7fd15f834264a1441c9519355e
Ohakwe-Love <ohakwemuna@gmail.com>
2025-09-15 03:30:42 +0100

    Updated homepage for Crown

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll(".background-slide");
+let currentSlide = 0;
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle("active", i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+
+
+setInterval(nextSlide, 5000);
